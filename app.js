016e29f00a71a6de35e0f198f960778634d371df
Jay <thk116@gmail.com>
2020-05-11 16:54:53 -0700

    updating work and changed sendgrid api key

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const morgan = require('morgan');
 
 const app = express();
 
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -14,8 +17,19 @@ app.get('/', (req, res) => {
     res.json('HELLO slash')
 })
 
-app.get('/contact', (req, res) => {
-    res.json('CONTACT slash')
+app.post('/contact', (req, res) => {
+    const msg = {
+        to: 'thk116@gmail.com',
+        from: 'jthk1106@gmail.com',
+        subject: 'Sending with Twilio SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    sgMail.send(msg)
+        .then(res => console.log('the RES: ', res))
+        .catch(err => console.log('the ERR: ', err))
+
+    res.json('email sent')
 })
 
 module.exports = app;
